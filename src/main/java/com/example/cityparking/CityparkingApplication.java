93dc8b3e4402267078db8a274b8c961125436f05
vitin <mateusz.wojtaszewski@gmail.com>
2018-02-14 13:39:14 +0100

    create spring boot project

---
+++ src/main/java/com/example/cityparking/CityparkingApplication.java
@@ -0,0 +1,12 @@
+package com.example.cityparking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityparkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityparkingApplication.class, args);
+	}
+}
